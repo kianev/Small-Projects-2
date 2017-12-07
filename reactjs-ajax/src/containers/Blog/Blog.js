d49f04e01e1d118c9fb09c89b0c30de60cbf5dab
Kaloian Ianev <kianev@gmail.com>
2017-12-07 13:46:14 +0200

    POST request with Axios to https://jsonplaceholder.typicode.com done.

--- reactjs-ajax/src/containers/Blog/Blog.js
+++ reactjs-ajax/src/containers/Blog/Blog.js
@@ -6,24 +6,46 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+  state = {
+    posts: [],
+    selectedPostId: null
+  }
 
   componentDidMount(){
     axios.get('https://jsonplaceholder.typicode.com/posts')
       .then(response => {
-        console.log(response);
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: "Pesho"
+          }
+        })
+        this.setState({posts: updatedPosts});
       });
   }
 
+  postSelectedHandler = (id) => {
+    this.setState({selectedPostId: id})
+  }
+
     render () {
+    const posts = this.state.posts.map(post => {
+      return  <Post
+        key={post.id}
+        title={post.title}
+        author={post.author}
+        clicked={() => this.postSelectedHandler(post.id)}
+      />
+    });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                  {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
