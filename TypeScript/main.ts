bcccdd69f939466225e220468bf5bb3126b2f451
Kaloian Ianev <kianev@gmail.com>
2018-03-14 15:01:10 +0200

    TypeScript constructor and optional parameters

--- TypeScript/main.ts
+++ TypeScript/main.ts
@@ -2,16 +2,15 @@ class Point {
     x: number;
     y: number;
 
+    constructor(x?: number, y?: number){
+        this.x = x;
+        this.y = y;
+    }
+
     draw() {
         console.log('X: ' + this.x + ' Y: ' + this.y);
     }
-
-    getDistance(another: Point){
-
-    }
 }
 
-let point = new Point();
-point.x = 1;
-point.y = 2;
+let point = new Point(1,2);
 point.draw();
